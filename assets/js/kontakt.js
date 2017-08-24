36c5e3e16d37eb9aa8ab3609e81eaf2c6a3d8985
IShowerNaked <ishowernaked404@googlemail.com>
2017-08-24 10:39:02 -0600

    contact form submission

--- assets/js/kontakt.js
+++ assets/js/kontakt.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    var sendTo = 'moc.liamg@tkcanehcsudhci' .split("").reverse().join("");
+    var sendTo = 'lp.2o@akslop-enueaz' .split("").reverse().join("");
 
     $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
@@ -21,11 +21,17 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
 
+            var sendText =
+                  "Name  :    " + name    + "\n"
+                + "E-Mail:    " + email   + "\n"
+                + "Telefon:   " + phone   + "\n"
+                + "Nachricht: " + message
+
             $.ajax({
                 url: "https://formspree.io/" + sendTo,
                 method: "POST",
                 data: JSON.stringify({
-                    message: message
+                    message: sendText
                 }),
                 contentType: "application/json",
                 dataType: "json",
@@ -36,7 +42,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Ihre Nachricht wurde versandt. Wir werden so schnell wie möglich Kontakt mit Ihnen aufnehmen.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
                     $('#contactForm').trigger("reset");
